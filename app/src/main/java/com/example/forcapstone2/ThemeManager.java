67fae242a233e3fd12eb54020a1d71ce6f12c99b
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-06-27 16:59:45 +0900

    2024-06-27

--- app/src/main/java/com/example/forcapstone2/ThemeManager.java
+++ app/src/main/java/com/example/forcapstone2/ThemeManager.java
@@ -1,9 +1,10 @@
-// ThemeManager.java
 package com.example.forcapstone2;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -11,19 +12,29 @@ public class ThemeManager {
     private Activity activity;
     private FrameLayout lightThemeLayout, darkThemeLayout;
     private Switch switch1, switch2;
+    private ImageView buttonSetting, statisticsIcon, bluetoothIcon, reloadIcon;
+    private TextView nowAmount, waterAmountText, soFarText, farText, settingTextView, bluetoothTextView, statsTextView, refreshTextView;
 
-    public ThemeManager(Activity activity) {
+    public ThemeManager(Activity activity, FrameLayout lightThemeLayout, FrameLayout darkThemeLayout, Switch switch1, Switch switch2, ImageView buttonSetting, ImageView statisticsIcon, ImageView bluetoothIcon, ImageView reloadIcon, TextView nowAmount, TextView waterAmountText) {
         this.activity = activity;
-    }
+        this.lightThemeLayout = lightThemeLayout;
+        this.darkThemeLayout = darkThemeLayout;
+        this.switch1 = switch1;
+        this.switch2 = switch2;
+        this.buttonSetting = buttonSetting;
+        this.statisticsIcon = statisticsIcon;
+        this.bluetoothIcon = bluetoothIcon;
+        this.reloadIcon = reloadIcon;
+        this.nowAmount = nowAmount;
+        this.waterAmountText = waterAmountText;
+        this.soFarText = soFarText;
+        this.farText = farText;
+        this.settingTextView = settingTextView;
+        this.bluetoothTextView = bluetoothTextView;
+        this.statsTextView = statsTextView;
+        this.refreshTextView = refreshTextView;
 
-    public void initializeThemes() {
-        lightThemeLayout = activity.findViewById(R.id.lightThemeLayout);
-        darkThemeLayout = activity.findViewById(R.id.darkThemeLayout);
-        switch1 = activity.findViewById(R.id.switch1);
-        switch2 = activity.findViewById(R.id.switch2);
-
-        // 초기 테마 설정
-        setInitialTheme();
+        setInitialTheme(); // Set the initial theme during initialization
     }
 
     private void setInitialTheme() {
@@ -31,6 +42,7 @@ public class ThemeManager {
         darkThemeLayout.setVisibility(View.GONE);
         switch1.setChecked(false);
         switch2.setChecked(false);
+        updateIconsForLightTheme();
     }
 
     public void switchToDarkTheme() {
@@ -38,6 +50,7 @@ public class ThemeManager {
         darkThemeLayout.setVisibility(View.VISIBLE);
         switch1.setChecked(true);
         switch2.setChecked(true);
+        updateIconsForDarkTheme();
     }
 
     public void switchToLightTheme() {
@@ -45,5 +58,40 @@ public class ThemeManager {
         darkThemeLayout.setVisibility(View.GONE);
         switch1.setChecked(false);
         switch2.setChecked(false);
+        updateIconsForLightTheme();
+    }
+
+    private void updateIconsForLightTheme() { // 라이트 모드
+        buttonSetting.setImageResource(R.drawable.settings);
+        statisticsIcon.setImageResource(R.drawable.chart);
+        bluetoothIcon.setImageResource(R.drawable.bluetooth);
+        reloadIcon.setImageResource(R.drawable.reload);
+
+        nowAmount.setTextColor(Color.parseColor("#194569"));
+        waterAmountText.setTextColor(Color.parseColor("#194569"));
+        farText.setTextColor(Color.parseColor("#5f84a2"));
+        soFarText.setTextColor(Color.parseColor("#5f84a2"));
+
+        settingTextView.setTextColor(Color.parseColor("#194569"));
+        bluetoothTextView.setTextColor(Color.parseColor("#194569"));
+        statsTextView.setTextColor(Color.parseColor("#194569"));
+        refreshTextView.setTextColor(Color.parseColor("#194569"));
+    }
+
+    private void updateIconsForDarkTheme() { // 다크 모드
+        buttonSetting.setImageResource(R.drawable.settings_w);
+        statisticsIcon.setImageResource(R.drawable.chart_w);
+        bluetoothIcon.setImageResource(R.drawable.bluetooth_w);
+        reloadIcon.setImageResource(R.drawable.reload_w);
+
+        nowAmount.setTextColor(Color.parseColor("#cadeed")); // 아이콘 밑에 글자 색상
+        waterAmountText.setTextColor(Color.parseColor("#cadeed"));
+        farText.setTextColor(Color.parseColor("#91aec4"));
+        soFarText.setTextColor(Color.parseColor("#91aec4"));
+
+        settingTextView.setTextColor(Color.parseColor("#cadeed")); //물 양 표기 색상
+        bluetoothTextView.setTextColor(Color.parseColor("#cadeed"));
+        statsTextView.setTextColor(Color.parseColor("#cadeed"));
+        refreshTextView.setTextColor(Color.parseColor("#cadeed"));
     }
 }
